0ff00704237a01648584baf782bec9c89cbb4954
karzuin <karzuin@yahoo.com>
2020-10-10 21:39:08 +0000

    Tidy up js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-//*Card data
+//Declaring card data
 const cardsArray = [
   {
     name: 'stormtrooper on sand',
@@ -33,74 +33,53 @@ const cardsArray = [
     name: 'stormtrooper laughing',
     img: 'assets/images/lego08.jpg',
   },
-]
+];
 
-//Code below taken from https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/ 
-//Where I modified the code it is marked with *.
-
-//variables 
+//Declaring global variables 
 
 let firstGuess = '';
 let secondGuess = '';
 let count = 0; //stores the count
 let previousTarget = null;
 let delay = 1200;
-// Record current timer state (on or off)
 let timerOn = false;
 let matchCount = 0;
 
-//duplicates the cards to make 16 and shuffles cards
+//DOM manipulation code below taken from https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/ 
+//Duplicates the cards to make 16 and shuffles cards
 let gameGrid = cardsArray.concat(cardsArray);
 gameGrid.sort(() => 0.5 - Math.random());
 
-//creating divs for card images, displays images, DOM manipulation
+//DOM manipulation - creating divs, and displaying card images
 let game = document.getElementById('game');
 let grid = document.createElement('section');
 grid.setAttribute('class', 'grid');
 game.appendChild(grid);
 
 gameGrid.forEach(function(item)  {
-  let name = item.name,
-      img = item.img;
 
-  let card = document.createElement('div');
-  card.classList.add('card');
-  card.dataset.name = name;
+    let name = item.name,
+    img = item.img;
 
-  let front = document.createElement('div');
-  front.classList.add('front');
+let card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.name = name;
 
-  let back = document.createElement('div');
-  back.classList.add('back');
-  back.style.backgroundImage = 'url(' + img + ')';
+let front = document.createElement('div');
+    front.classList.add('front');
 
-  //appends divs to the grid
-  grid.appendChild(card);
-  card.appendChild(front);
-  card.appendChild(back);
-    });
+let back = document.createElement('div');
+    back.classList.add('back');
+    back.style.backgroundImage = 'url(' + img + ')';
 
-//declaring shuffle function code taken from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-/*function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+//Appends divs to the grid
+    grid.appendChild(card);
+    card.appendChild(front);
+    card.appendChild(back);
+});
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}*/
-
-//*declaring match function
+//Match function code below taken from https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/ 
+//Declaring match function
 let match = function match() {
     matched = document.querySelectorAll('.selected');
     matched.forEach(function (card) {
@@ -108,32 +87,34 @@ let match = function match() {
   });
 };
 
-//declaring reset cards function
+//Declaring reset card and selected function code below taken from https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/ 
+//Declaring reset cards function
 let resetGuesses = function resetGuesses() {
-  firstGuess = '';
-  secondGuess = '';
-  count = 0;
-  previousTarget = null;
+    firstGuess = '';
+    secondGuess = '';
+    count = 0;
+    previousTarget = null;
 
-  let selected = document.querySelectorAll('.selected');
-  selected.forEach(function (card) {
+let selected = document.querySelectorAll('.selected');
+    selected.forEach(function (card) {
     card.classList.remove('selected');
   });
 };
 
-//*counts the number of moves
+//Counts the number of moves
 let moves = 0;
 let counter = document.querySelector(".moves");
-//* move counter function
+
+//Move counter function
 function moveCounter() {
     moves++;
     counter.innerHTML = moves + ' moves'; 
 };
 
-//*timer
+//Timer
 let time = 0;
 let timer; //stores the setInterval
-    function startTimer() {
+function startTimer() {
     timer = setInterval(function() {
     time++;
     minutes = ("0" + Math.floor(time / 60)).slice(-2);
@@ -142,35 +123,40 @@ let timer; //stores the setInterval
   }, 1000);
 }
 
-//function for finishing game
+//Victory popup for finishing game
  function victoryPopUp(moves, time) {
-  let popUp = document.querySelector('.popUp');
-  popUp.style.visibility = "visible";
-  popUp.querySelector('.popUpTime').innerHTML = "You won the game in " + minutes + " mins and " + seconds + " secs!";
-  popUp.querySelector('.popUpMoves').innerHTML = "You made " + moves + " moves!"
+    let popUp = document.querySelector('.popUp');
+        popUp.style.visibility = "visible";
+        popUp.querySelector('.popUpTime').innerHTML = "You won the game in " + minutes + " mins and " + seconds + " secs!";
+        popUp.querySelector('.popUpMoves').innerHTML = "You made " + moves + " moves!"
 };
 
-//create click event listener, calling match, reset functions
+//Event listener code below taken from https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/ and modified
+//Create click event listener, calling match, reset functions
 grid.addEventListener('click', function (event) {
 
   let clicked = event.target; //any element that has been clicked 
-   // *Start the timer on the first click
+   //Start the timer on the first click
     if (timerOn === false) {
-      startTimer();
-      timerOn = true;
+        startTimer();
+        timerOn = true;
     }
 
-  if (clicked.nodeName === 'SECTION' || //selects divs inside the grid section
-    clicked === previousTarget || //ignore if the same card is clicked again
-    clicked.parentNode.classList.contains('selected') || //stops already selected cards to flip over
-    clicked.parentNode.classList.contains('match')) //stops matched cards to flip over if clicked again
-     {
-    return; // stops function
+//Stop function code below taken from https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/ 
+
+    if (clicked.nodeName === 'SECTION' || //selects divs inside the grid section
+        clicked === previousTarget || //ignore if the same card is clicked again
+        clicked.parentNode.classList.contains('selected') || //stops already selected cards to flip over
+        clicked.parentNode.classList.contains('match')) //stops matched cards to flip over if clicked again
+        {
+    return; //stops function
   }
 
+//Count and select code below taken from https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/ and modified
+
   if (count < 2) {
-    count++;
-    moveCounter(); //*to count number of moves at first click
+        count++;
+        moveCounter(); //to count number of moves at first click
     if (count === 1) {
       firstGuess = clicked.parentNode.dataset.name;
       clicked.parentNode.classList.add('selected');
@@ -179,48 +165,34 @@ grid.addEventListener('click', function (event) {
       clicked.parentNode.classList.add('selected');
     }
 
-    // call match function, delays for reset
+//Compare for match and reset code below taken from https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/ 
+//and modified
+
+    //Call match function, delays for reset
     if (firstGuess && secondGuess) {
       if (firstGuess === secondGuess) {
         matchCount++
-        match(); //* calling match function modified as no need for delay, keeping matched cards visible
+        match(); //calling match function modified as no need for delay, keeping matched cards visible
       }
-      setTimeout(resetGuesses, delay);
+        setTimeout(resetGuesses, delay);
     }
-    previousTarget = clicked;
-  }
-  // Check for the winning condition:
-    if (matchCount === 8) {
-      victoryPopUp(moves, time);
-      clearInterval(timer);
-      timerOn = false;
+        previousTarget = clicked;
+};
+  
+//Check for the winning condition
+if (matchCount === 8) {
+    victoryPopUp(moves, time);
+    clearInterval(timer);
+    timerOn = false;
     }
 });
 
-//*declaring restart game function
+//Restart game
 function reloadGame(){
     window.location.reload();
 } 
 
 let startGame = function startGame() {
-   reloadGame();
-    
-    //stops and resets moves
-    /*moves = 0;
-    document.querySelector('.moves').innerHTML = moves + ' moves';
-    
-    //stops timer
-   /* time = 0;
-    minutes = 0;
-    seconds = 0; 
-    document.querySelector('.timer').innerHTML = "0" + minutes + ":" + "0" + seconds;
-    clearInterval(timer);
-    timerOn = false; //stops timer */
-    
-    //flips over matched cards
-   /* let resetMatched = document.querySelectorAll('.match');
-    resetMatched.forEach(function (card) {
-    card.classList.remove('match');
-  });*/
-}
+    reloadGame();
+};
 
